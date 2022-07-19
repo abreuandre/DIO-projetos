a59127963cdc2d79f4fc50a9ff06ca558f632cdd
abreuandre <andremachadofbn@gmail.com>
2022-07-19 09:52:26 -0300

    Inclusão das atualizações JavaScript

---
+++ Introdução ao JavaScript/TESTE/teste.js
@@ -0,0 +1,22 @@
+//var preco = 2;
+var desconto = 0.2;
+const PRECO = 2;
+var total = PRECO - desconto;
+
+
+//Funções
+
+function soma(a, b) { //declaração de uma função
+    console.log(a + b);
+    return a + b;
+}
+
+soma(3, 5); //chamada de uma função
+
+// Console
+
+// Com o atalho ctrl + shift + i, vamos abrir a inspeção e após clicar em Console.
+
+// Podemos trabalhar o JavaScript no console do navegador(cliente) ou pelo Node.JS(Servidor).
+
+console.log("Hello World!");
